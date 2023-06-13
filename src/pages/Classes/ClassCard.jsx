4af0d716262f8cb3549b1b2d7c0391a5bb044bdd
ForhadRoben <i.rforhad@gmail.com>
2023-06-13 15:17:35 +0600

    classes page added

--- src/pages/Classes/ClassCard.jsx
+++ src/pages/Classes/ClassCard.jsx
@@ -1,8 +1,18 @@
 
 const ClassCard = ({ item }) => {
+    const { image, course_name, available_seats, instructors, course_price } = item;
     return (
-        <div>
-
+        <div className="card lg:card-side bg-red-200 w-full mx-auto mt-8">
+            <img className="w-48 h-60 rounded" src={image} alt="instructor_image" />
+            <div className="card-body">
+                <h2 className="card-title">{course_name}</h2>
+                <p >Instructor: {instructors[0].instructor_name}</p>
+                <p >Available Seats: {available_seats}</p>
+                <p >Course Price: {course_price}</p>
+                <div className="card-actions ">
+                    <button className="text-white btn btn-error">Select</button>
+                </div>
+            </div>
         </div>
     );
 };
