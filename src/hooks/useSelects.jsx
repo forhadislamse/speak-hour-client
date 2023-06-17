b7a67f88a191cb75cb809495d362de99ef77fe71
ForhadRoben <i.rforhad@gmail.com>
2023-06-17 13:12:53 +0600

    axios secure implemented

--- src/hooks/useSelects.jsx
+++ src/hooks/useSelects.jsx
@@ -1,14 +1,29 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
+import useAxiosSecure from './useAxiossSecure';
 const useSelects = () => {
-    const { user } = useContext(AuthContext);
-
+    const { user, loading } = useContext(AuthContext);
+    const [axiosSecure] = useAxiosSecure();
+    // const token = localStorage.getItem('access-token');
     const { refetch, data: select = [] } = useQuery({
         queryKey: ['selects', user?.email],
+
+        enabled: !loading,
+        // queryFn: async () => {
+        //     const res = await fetch(`http://localhost:5000/selects?email=${user?.email}`, {
+        //         headers: {
+        //             authorization: `bearer ${token}`
+
+        //         }
+        //     })
+        //     return res.json();
+        // },
+
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/selects?email=${user?.email}`)
-            return res.json();
+            const res = await axiosSecure(`/selects?email=${user?.email}`)
+            console.log('res from axios', res)
+            return res.data;
         },
     })
 
